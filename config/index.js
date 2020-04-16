48fd4bde11ce7a07cd9d2554a7fb6608833d135c
Steven Hunt <steven.mtw.hunt@gmail.com>
2020-04-16 08:28:14 -0400

    update default text in editors

--- config/index.js
+++ config/index.js
@@ -1,10 +1,20 @@
 // configuration for testjam.io
 
+const stepSource = "// Step definition file:\n\n" +
+  "const { Given, When, Then } = require('cucumber'); // load CucumberJS based on selected runtime version.\n" +
+  "const assert = require('assert'); // imported modules are automatically downloaded using browserify-cdn.\n\n" +
+  "Given('my step definition runs...', function () {\n    // step definition code.\n});\n";
+
+const featureSource = "# Gherkin feature file:\n\n" +
+  "Feature: my feature\n\n" +
+  "  Scenario: my scenario\n" +
+  "  Given my step definition runs...";
+
 module.exports = {
   runtimes: require('./runtimes.json'),
   themes: require('./themes.json'),
   firebase: require('./firebase.json'),
   strings: require('./lang/en-US.json'),
-  featureSource: '# Welcome to gherkin! Add features and scenarios here.',
-  stepSource: '// Welcome! You can require packages at the top of your code just like in NodeJS.'
+  featureSource: featureSource,
+  stepSource: stepSource
 };
